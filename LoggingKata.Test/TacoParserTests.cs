4bc103fa5fdaedb08859b7e63febbf5ce6ad94eb
Brody May <brodymay2019@outlook.com>
2023-02-28 14:08:57 -0500

    group assignment finished

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -23,6 +23,9 @@ namespace LoggingKata.Test
 
         [Theory]
         [InlineData("34.073638, -84.677017, Taco Bell Acwort...", -84.677017)]
+        [InlineData("34.087508,-84.575512,Taco Bell Acworth", -84.575512)]
+        [InlineData("33.671982,-85.826674,Taco Bell Annisto", -85.826674)]
+        [InlineData("33.858498,-84.60189,Taco Bell Austel", -84.60189)]
         public void ShouldParseLongitude(string line, double expected)
         {
             // TODO: Complete - "line" represents input data we will Parse to
@@ -30,14 +33,30 @@ namespace LoggingKata.Test
             //       each representing a TacoBell location
 
             //Arrange
+            var tacoParser = new TacoParser();
 
             //Act
+            var actual = (tacoParser.Parse(line));
 
             //Assert
+            Assert.Equal(expected, actual.Location.Longitude);
         }
 
 
         //TODO: Create a test ShouldParseLatitude
+        [Theory]
+        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", 34.073638)]
+        [InlineData("34.087508,-84.575512,Taco Bell Acworth", 34.087508)]
+        [InlineData("33.671982,-85.826674,Taco Bell Annisto", 33.671982)]
+        [InlineData("30.903723,-84.556037,Taco Bell Bainbridg", 30.903723)]
 
+        public void ShouldParseLatitude(string line, double expected)
+        { 
+            var tacoParser = new TacoParser();
+
+            var actual = tacoParser.Parse(line);
+
+            Assert.Equal(expected, actual.Location.Latitude);
+        }
     }
 }
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -18,15 +18,19 @@
             if (cells.Length < 3)
             {
                 // Log that and return null
+                logger.LogWarning("less than three items.  incomplete data");
                 // Do not fail if one record parsing fails, return null
                 return null; // TODO Implement
             }
 
             // grab the latitude from your array at index 0
+            var latitude = double.Parse(cells[0]);
             // grab the longitude from your array at index 1
+            var longitude = double.Parse(cells[1]);
             // grab the name from your array at index 2
-
+            var name = cells[2];
             // Your going to need to parse your string as a `double`
+
             // which is similar to parsing a string as an `int`
 
             // You'll need to create a TacoBell class
@@ -38,7 +42,16 @@
             // Then, return the instance of your TacoBell class
             // Since it conforms to ITrackable
 
-            return null;
+            var points = new Point();
+            points.Latitude = latitude;
+            points.Longitude = longitude;
+            
+            var tacoBell = new TacoBell();
+            tacoBell.Name= name;
+
+            tacoBell.Location = points;
+
+            return tacoBell;
         }
     }
 }
